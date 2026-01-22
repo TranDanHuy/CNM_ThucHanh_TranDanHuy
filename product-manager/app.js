4a3ef718bcae0d219763c04076c73ea682b47dce
TranDanHuy <156204176+TranDanHuy@users.noreply.github.com>
2026-01-22 18:54:28 +0700

    commit công nghệ mới thực hành lab 3

---
+++ product-manager/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", require("./routes/product.routes"));
+
+app.listen(3000, () =>
+  console.log("Server running on port 3000")
+);
